2a5b425f907ab74de210a9b395278aa10b96cd50
baihe <baihe@xiaomai5.com>
2020-02-27 09:49:46 +0800

    update annotation

--- src/main/java/org/baihe/data/repo/RepoServiceConfig.java
+++ src/main/java/org/baihe/data/repo/RepoServiceConfig.java
@@ -15,9 +15,18 @@
 
 package org.baihe.data.repo;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author baihe Created on 2020/2/26 8:24 PM
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface RepoServiceConfig {
   String schema() default "";
   String table() default "";
